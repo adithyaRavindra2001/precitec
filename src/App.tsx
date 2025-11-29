a6b9b46765feba06999a014e79d9b9d023813370
Anirudha Acharya <anirudhbacharya@gmail.com>
2025-11-29 17:37:32 +0530

    Add ScrollToTop component and integrate it into App layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-import { SiteFooter } from "@/components/layout/SiteFooter"
-import { SiteHeader } from "@/components/layout/SiteHeader"
-import { LandingPage } from "@/pages/LandingPage"
-import { MachineDetail } from "@/pages/MachineDetail"
-import { Products } from "@/pages/Products"
-import { ProductDetailPage } from "@/pages/ProductDetailPage"
-import { CategoryPage } from "@/pages/CategoryPage"
-import { Outlet, Route, Routes } from "react-router-dom"
+import { ScrollToTop } from "@/components/layout/ScrollToTop";
+import { SiteFooter } from "@/components/layout/SiteFooter";
+import { SiteHeader } from "@/components/layout/SiteHeader";
+import { LandingPage } from "@/pages/LandingPage";
+import { MachineDetail } from "@/pages/MachineDetail";
+import { Products } from "@/pages/Products";
+import { ProductDetailPage } from "@/pages/ProductDetailPage";
+import { CategoryPage } from "@/pages/CategoryPage";
+import { Outlet, Route, Routes } from "react-router-dom";
 
 function Layout() {
   return (
@@ -16,24 +17,33 @@ function Layout() {
       </main>
       <SiteFooter />
     </div>
-  )
+  );
 }
 
 function App() {
   return (
-    <Routes>
-      <Route element={<Layout />}>
-        <Route index element={<LandingPage />} />
-        {/* New Products Routes */}
-        <Route path="/products" element={<Products />} />
-        <Route path="/products/:productId" element={<ProductDetailPage />} />
-        <Route path="/products/category/:categoryId" element={<CategoryPage />} />
-        <Route path="/products/category/:categoryId/:subcategoryId" element={<CategoryPage />} />
-        {/* Legacy Routes - Keep for backward compatibility */}
-        <Route path="/machine/:id" element={<MachineDetail />} />
-      </Route>
-    </Routes>
-  )
+    <>
+      <ScrollToTop />
+      <Routes>
+        <Route element={<Layout />}>
+          <Route index element={<LandingPage />} />
+          {/* New Products Routes */}
+          <Route path="/products" element={<Products />} />
+          <Route path="/products/:productId" element={<ProductDetailPage />} />
+          <Route
+            path="/products/category/:categoryId"
+            element={<CategoryPage />}
+          />
+          <Route
+            path="/products/category/:categoryId/:subcategoryId"
+            element={<CategoryPage />}
+          />
+          {/* Legacy Routes - Keep for backward compatibility */}
+          <Route path="/machine/:id" element={<MachineDetail />} />
+        </Route>
+      </Routes>
+    </>
+  );
 }
 
-export default App
+export default App;
